ae43d8c87223a876e6630704b488eea3a6f3983e
Adam Williams <guswillad@student.gu.se>
2024-06-15 13:20:36 +0200

    homepage/notecomponent
    finished building the basic homepage and notecomponent

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,6 +3,7 @@ import {jwtDecode} from "jwt-decode";
 import api from "../api";
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants";
 import { useState, useEffect } from "react";
+import LoadingIndicator from "./LoadingIndicator";
 
 function ProtectedRoute({ children }) {
     const [isAuthorized, setIsAuthorized] = useState(null);
@@ -49,7 +50,7 @@ function ProtectedRoute({ children }) {
     }
 
     if (isAuthorized === null) {
-        return <div>Loading...</div>
+        return loading && <LoadingIndicator/>
     }
 
     return isAuthorized ? children : <Navigate to="/login" /> //if user is authorized return children, else redirect to login page
