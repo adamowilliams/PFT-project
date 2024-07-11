d66aba14030546828fca14765d3e89c51c0ebe22
iorthethug <adam-williams@live.se>
2024-07-11 15:36:14 +0200

    deployment setup

--- backend/backend/settings copy.py
+++ backend/backend/settings copy.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
+SECRET_KEY = 'django-insecure-r=lvjsc-)xpq!y6hon$@ap^69wfl^y6e+m5sds3j5%bjj9^tf&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS =  ["*"] 
 
@@ -61,7 +61,6 @@ INSTALLED_APPS = [
     "notes",
     "finance_tracker",
     "ml_app",
-    "frontend",
 ]
 
 MIDDLEWARE = [
@@ -142,8 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
